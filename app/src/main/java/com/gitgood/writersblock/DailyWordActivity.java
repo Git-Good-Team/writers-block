516037bb253c299e003110347e54b8db58350984
Rafael Oliveira <bellotti.rafael@gmail.com>
2020-02-26 17:32:03 -0800

    organizes definitions and dinamically adds them to screen

--- app/src/main/java/com/gitgood/writersblock/DailyWordActivity.java
+++ app/src/main/java/com/gitgood/writersblock/DailyWordActivity.java
@@ -2,10 +2,18 @@ package com.gitgood.writersblock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -43,10 +51,10 @@ public class DailyWordActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<DailyWord> call, Response<DailyWord> response) {
                 TextView dailyWordTextView = findViewById(R.id.daily_word_text_view);
-                TextView definitionTextView = findViewById(R.id.daily_word_definition);
 
                 dailyWordTextView.setText(response.body().getWord());
-                definitionTextView.setText(response.body().getDefinitions().get(0).getDefinition());
+                HashMap<String, List<String>> sortedDefinitions = sortDefinitions(response.body().getDefinitions());
+                addDefinitionsToActivity(sortedDefinitions);
             }
 
             @Override
@@ -55,5 +63,61 @@ public class DailyWordActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(), R.string.api_connection_error_message, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private HashMap<String, List<String>> sortDefinitions(List<DailyWordDefinition> definitions) {
+        HashMap<String, List<String>> definitionMap = new HashMap<>();
+
+        for (DailyWordDefinition definition : definitions) {
+            List<String> definitionSet = definitionMap.getOrDefault(definition.getPartOfSpeech(), new ArrayList<String>());
+            definitionSet.add(definition.getDefinition());
+            definitionMap.put(definition.getPartOfSpeech(), definitionSet);
+        }
+
+        return definitionMap;
+    }
+
+    private void addDefinitionsToActivity(HashMap<String, List<String>> definitionsMap) {
+        Iterator iterator = definitionsMap.entrySet().iterator();
+        LinearLayout layout = findViewById(R.id.daily_word_layout);
+
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            addPartOfSpeechTextView(layout, (String) pair.getKey());
+
+            int position = 1;
+            for (String definition : (List<String>) pair.getValue()) {
+                addDefinitionTextView(layout, definition, position);
+                position++;
+            }
+        }
+    }
+
+    private void addPartOfSpeechTextView(LinearLayout layout, String partOfSpeech) {
+        TextView partOfSpeechTextView = new TextView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.setMargins(10, 0, 0, 10);
+
+        partOfSpeechTextView.setLayoutParams(layoutParams);
+        partOfSpeechTextView.setText(partOfSpeech);
+        partOfSpeechTextView.setTextColor(getResources().getColor(R.color.color_standard_text));
+        partOfSpeechTextView.setTypeface(null, Typeface.ITALIC);
+
+        layout.addView(partOfSpeechTextView);
+    }
+
+    private void addDefinitionTextView(LinearLayout layout, String definition, int position) {
+        TextView definitionTextView = new TextView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.setMargins(10, 0, 0, 10);
+        definitionTextView.setLayoutParams(layoutParams);
+        definitionTextView.setTextColor(getResources().getColor(R.color.color_standard_text));
+        definitionTextView.setText(position + ". " + definition);
+
+        layout.addView(definitionTextView);
     }
 }
